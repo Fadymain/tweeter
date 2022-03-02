134cae52f8b80de817aaa00cf007ae05ddb894bc
Adetola Fadairo <adetolafadairo@gmail.com>
2022-03-02 23:17:15 +0000

    creating a post request with ajax through the function laodTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,30 +6,30 @@
 
 $(document).ready(function () {
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 const renderTweets = function(tweets) {
 // loops through tweets
@@ -46,11 +46,25 @@ $("#tweetcent").submit(function(event) {
   const serializeData = $(this).serialize();
   console.log("serializedData", serializeData);
   $.post("/tweets/", serializeData, (response) => {
-    
+    loadTweets();
     console.log("response", response);
   })
 })
 
+const loadTweets = function() {
+  $.ajax({
+    url: "/tweets/",
+    method: "GET",
+    success: (data) => {
+      console.log("data", data);
+      renderTweets(data);
+    },
+    error: (err) => {
+      console.log("err", err);
+    }
+  })
+}
+loadTweets();
 
 const createTweetElement = function(tweet) {
 let $tweet = `
