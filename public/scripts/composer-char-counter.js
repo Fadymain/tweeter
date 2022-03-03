213848d8a60826134a7e0ecf89f3e0f30519e74e
Adetola Fadairo <adetolafadairo@gmail.com>
2022-03-03 03:12:34 +0000

    refactoring

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $("#tweet-text").on("input", function(event) {
-    console.log(event);
-    console.log($(this).val())
-    console.log($(this).val().length)
+    // console.log(event);
+    // console.log($(this).val())
+    // console.log($(this).val().length)
     const wordCount = $(this).val().length;
     if (wordCount > 140) {
       document.querySelector(".counter").classList.add("redCounter")
